47f14a67edaeaa4dbd1216009a9fd80f382e9353
Apple <pkwok@hotmail.com>
2018-06-12 06:24:08 +0800

    modified

--- client/pages/Insert/Insert.js
+++ client/pages/Insert/Insert.js
@@ -13,9 +13,66 @@ Page({
     APII_title: "APII First TestPage",
     sec1:"1",
     sec2:"2",
-    sec3:"3"
+    sec3:"3",
+    counter1:0,
+    counter2:0,
+    response:"OK"
   },
   
+  updateText: function (e, text1) {
+    this.setData({
+      response: text1
+    })
+  },
+
+  addButton1: function (e) {
+    counter1: 0
+    this.setData({
+      counter1: this.counter1+1
+    })
+    console.log('counter1:', this.counter1)
+  },
+
+  minusButton1: function (e) {
+    counter1: 0
+    this.setData({
+      counter1: this.counter1-1
+    })
+    console.log('counter1:', this.counter1)
+  },
+
+  addButton2: function (e) {
+    counter2: 0
+    this.setData({
+      counter2: this.counter2+1
+    })
+    console.log('counter2:', this.counter2)
+  },
+
+  minusButton2: function (e) {
+    counter2: 0
+    this.setData({
+      counter2: this.counter2-1
+    })
+    console.log('counter2:', this.counter2)
+  },
+
+  submitButton: function (e) {
+    wx.request({
+      url: 'http://44ln6hzr.qcloud.la/weapp/insert', 
+      data: {
+        x: this.counter1,
+        y: this.counter2
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log('Value Inserted')
+      }
+    })
+  },
+
   formSubmit: function (e) {
     //console.log('form发生了submit事件，携带数据为：', e.detail.value)
 
