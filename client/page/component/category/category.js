123e068f969fdc9fc446ac8a7eb33ee0fcb208e5
Paul Kwok <pkwok@hotmail.com>
2018-06-22 15:49:37 +0800

    modify category

--- client/page/component/category/category.js
+++ client/page/component/category/category.js
@@ -6,10 +6,15 @@ Page({
       { cata_id: '2', cata_desc: '飲料', cata_type: 'Main' },
       { cata_id: '999', cata_desc: '其他', cata_type: 'Main'}
     ],
-    detail: [],
+    detail: [
+      { item_pic_url: 'https://44ln6hzr.qcloud.la/images/menuitems/0.jpeg', item_short_desc: '宫保鸡丁', item_price: '50' },
+      { item_pic_url: 'https://44ln6hzr.qcloud.la/images/menuitems/1.jpeg', item_short_desc: '生炒排骨', item_price: '40' },
+      { item_pic_url: 'https://44ln6hzr.qcloud.la/images/menuitems/2.jpeg', item_short_desc: '土雞蛋炒蝦仁', item_price: '35' },
+      { item_pic_url: 'https://44ln6hzr.qcloud.la/images/menuitems/3.jpeg', item_short_desc: '椒鹽九肚魚', item_price: '60' }
+    ],
     curIndex: 0,
     isScroll: false,
-    toView: 'zc0'/*默认定位到哪个view*/
+    toView: '0'/*默认定位到哪个view*/
   },
 
 
@@ -18,13 +23,13 @@ Page({
     wx.request({
       url: 'http://44ln6hzr.qcloud.la/weapp/selectCata', 
       success(res) {
-        console.log(res.data.data)
-        console.log(that.data.category)
+        //console.log(res.data.data)
+        //console.log(that.data.category)
         that.setData({
           //detail: res.data
           category: res.data.data
         })
-        console.log(that.data.category)
+        //console.log(that.data.category)
       }
     });
   },
