594d2fec71180dff495b09a7fffbb99d7fcd40cc
Paul Kwok <pkwok@hotmail.com>
2018-07-20 16:37:47 +0800

    update category

---
+++ server/controllers/selectCataItem.js
@@ -0,0 +1,13 @@
+const { mysql } = require('../qcloud')
+const uuid = require('node-uuid')
+
+module.exports = async ctx => {
+  var id = uuid.v1()
+  var c_index = 1
+  var res = await mysql("apii_ItemMenu").select('item_pic_url', 'item_short_desc', 'item_price').where({item_main_cata: c_index}).then(res => {
+    console.log(res)
+    ctx.state.data = res;
+  })
+
+  //ctx.state.data = "ok" 
+}
