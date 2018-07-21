cdd1203a5e9d6e269f5923e0893cbd18be11e764
Apple <pkwok@hotmail.com>
2018-07-21 15:59:57 +0800

    modify add cart

--- client/page/component/cart/cart.js
+++ client/page/component/cart/cart.js
@@ -10,123 +10,54 @@ Page({
     curIndex: 0,
     isScroll: true,
     counter1: 0,
-    toView: '0'/*默认定位到哪个view*/
+    toView: '0',/*默认定位到哪个view*/
+    signature: '',
+    userInfo: '',
+    totalPrice: 0
   },
 
-  onReady() {
+  onShow () {
     var that = this;
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                signature: res.signature,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
     wx.request({
       url: 'http://44ln6hzr.qcloud.la/weapp/selectOrderView',
+      data: {
+        signature: that.data.signature
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
       success(res) {
-        //console.log(res.data.data)
-        //console.log(that.data.category)
         that.setData({
-          //detail: res.data
           cartsitem: res.data.data
         })
-        //console.log(that.data.category)
       }
     });
+    that.getTotalPrice();
   },
 
-  /**
-   * 当前商品选中事件
-   */
-  selectList(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    const selected = carts[index].selected;
-    carts[index].selected = !selected;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 删除购物车当前商品
-   */
-  deleteList(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    carts.splice(index, 1);
-    this.setData({
-      carts: carts
-    });
-    if (!carts.length) {
-      this.setData({
-        hasList: false
-      });
-    } else {
-      this.getTotalPrice();
-    }
-  },
-
-  /**
-   * 购物车全选事件
-   */
-  selectAll(e) {
-    let selectAllStatus = this.data.selectAllStatus;
-    selectAllStatus = !selectAllStatus;
-    let carts = this.data.carts;
-
-    for (let i = 0; i < carts.length; i++) {
-      carts[i].selected = selectAllStatus;
-    }
-    this.setData({
-      selectAllStatus: selectAllStatus,
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 绑定加数量事件
-   */
-  addCount(e) {
-    const index = e.currentTarget.dataset.index;
-    let carts = this.data.carts;
-    let num = carts[index].num;
-    num = num + 1;
-    carts[index].num = num;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 绑定减数量事件
-   */
-  minusCount(e) {
-    const index = e.currentTarget.dataset.index;
-    const obj = e.currentTarget.dataset.obj;
-    let carts = this.data.carts;
-    let num = carts[index].num;
-    if (num <= 1) {
-      return false;
-    }
-    num = num - 1;
-    carts[index].num = num;
-    this.setData({
-      carts: carts
-    });
-    this.getTotalPrice();
-  },
-
-  /**
-   * 计算总价
-   */
   getTotalPrice() {
-    let carts = this.data.carts;                  // 获取购物车列表
+    var that = this;
+    let carts = that.data.cartsitem;                  // 获取购物车列表
     let total = 0;
     for (let i = 0; i < carts.length; i++) {         // 循环列表得到每个数据
-      if (carts[i].selected) {                     // 判断选中才会计算价格
-        total += carts[i].num * carts[i].goodPrice;   // 所有价格加起来
-      }
+      total += carts[i].order_qty * carts[i].item_price;   // 所有价格加起来
     }
     this.setData({                                // 最后赋值到data中渲染到页面
-      carts: carts,
       totalPrice: total.toFixed(2)
     });
   }
--- server/controllers/selectOrderView.js
+++ server/controllers/selectOrderView.js
@@ -9,8 +9,8 @@ const uuid = require('node-uuid')
 
 module.exports = async ctx => {
   var id = uuid.v1()
-
-  var res = await mysql("apii_Orderview").select('cin_custname','order_qty','item_price','amount','item_short_desc','item_long_desc').then(res => {
+  var c_signature = ctx.query.signature
+  var res = await mysql("apii_Orderview").select('cin_custname', 'order_qty', 'item_price', 'amount', 'item_short_desc', 'item_long_desc').where({cin_custname: c_signature}).then(res => {
     console.log(res)
     ctx.state.data = res;
   })
